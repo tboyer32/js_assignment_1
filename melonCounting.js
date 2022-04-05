4e8637fb5db40609783d6b3f28e3d9d07b30bb8c
Tiffany Boyer <tboyercode@gmail.com>
2022-04-04 21:57:42 -0500

    finish assignment

---
+++ melonCounting.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+const countMelons = (melonList) => {
+    //Take in a list and return an object of # of melons by melon type.
+
+    const melonCounts = {};
+
+    for (const melon in melonList) {
+        if (melonList[melon] in melonCounts){
+            melonCounts[melonList[melon]] += 1;
+        } else {
+            melonCounts[melonList[melon]] = 1
+        }
+    }
+
+    return melonCounts;
+
+}
